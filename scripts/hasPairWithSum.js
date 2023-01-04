bc156ae5acf54d1e06e1619d2097b4b484ce442f
Ayxa Chaverra <achaverrar@gmail.com>
2023-01-04 17:47:39 -0500

    add more optimal solutions to hasPairWithSum and correct assumptions about the problem

--- scripts/hasPairWithSum.js
+++ scripts/hasPairWithSum.js
@@ -5,7 +5,7 @@ determines whether there is a matching pair whose
 sum equals a number that's also provided as input.
 
 Assumptions:
-- Numbers are integers
+- Numbers are integers (including zero and negative integers)
 - Numbers in the matching pair must have different indexes
 
 Examples:
@@ -28,6 +28,7 @@ Time complexity: O(n^2)
 Space complexity: O(1)
 */
 
+// Implementation 1
 function nHasPairWithSum1(arr, sum) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = i + 1; j < arr.length; j++) {
@@ -37,6 +38,7 @@ function nHasPairWithSum1(arr, sum) {
   return false;
 }
 
+// Implementation 2
 function nHasPairWithSum2(arr, sum) {
   arr.forEach((num1) => {
     arr.forEach((num2) => {
@@ -46,4 +48,35 @@ function nHasPairWithSum2(arr, sum) {
     });
   });
   return false;
+}
+
+// More Optimal Solutions
+/* Take the first number in the array and determine which
+would be its match to complete the desired sum and store
+that value in an object/set. 
+Verify if the next number in the array is in the object/set.
+- If so, return true. Otherwise, repeat the process used for 
+the first number in the array. 
+- If no matching pair is found, return false.
+
+Time complexity: O(n)
+Space complexity: O(1)
+*/
+
+// Implementation 1
+function oHasPairWithSum1(arr, sum) {
+  const matches = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] in matches) return true;
+    matches[sum - arr[i]] = true;
+  }
+}
+
+// Implementation 2
+function oHasPairWithSum2(arr, sum) {
+  const matches = new Set();
+  for (let i = 0; i < arr.length; i++) {
+    if (matches.has(arr[i])) return true;
+    matches.add(sum - arr[i]);
+  }
 }
